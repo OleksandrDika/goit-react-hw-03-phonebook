5f1b7f8c3216a5962f53dd6577eedb7882e3044b
OleksandrDika <109437683+OleksandrDika@users.noreply.github.com>
2023-02-28 20:01:43 +0200

    make hw3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,16 +4,30 @@ import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 import { Conteiner } from './Conteiner.styled';
+import startContacts from './contacts.json';
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const localStorCont = localStorage.getItem('contacts');
+    console.log(localStorCont);
+    if (localStorCont !== null) {
+      const parseCont = JSON.parse(localStorCont);
+      this.setState({ contacts: parseCont });
+      return;
+    }
+    console.log(startContacts);
+    this.setState({ contacts: startContacts });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   changeFilter = event => {
     this.setState({ filter: event.currentTarget.value });
